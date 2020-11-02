45b5c520935718b1e96b0fae7dbdd6e38d636bce
victoria santos <vivimarques10@hotmail.com>
2020-11-02 15:45:33 -0300

    inicio main

--- main.py
+++ main.py
@@ -3,15 +3,30 @@ import base_dados as bd
 
 total_dias = 5
 populacao = list()
-for i in range(0, 10):
+taxa_mutacao = 0.05
+taxa_cruzamento = 0.8
+nInd = 10
+for i in range(0, nInd):
     populacao.append(ag.ini_populacao(total_dias, 0))
 
 
-fitness = ag.funcao_fitness(populacao)
-pais = ag.funcao_dizimacao(populacao, fitness)
-filhos = ag.cruzamento(pais, 0.8)
-filhos_mutado = ag.mutacao(filhos, 0.05)
+geracao_atual = populacao
 
+for i in range(0, 50):
+
+    fitness = ag.funcao_fitness(geracao_atual)
+
+    nova_populacao = []
+    for k in range(0, int(nInd/2)):
+        pais = ag.funcao_dizimacao(geracao_atual, fitness)
+        filhos_gerados = ag.cruzamento(pais, taxa_cruzamento)
+        filhos_mutados = ag.mutacao(filhos_gerados, taxa_mutacao)
+        nova_populacao.append(filhos_mutados)
+
+        geracao_atual = nova_populacao
+
+fitness = ag.funcao_fitness(geracao_atual)
+print("\n-=-=-=--=-=-=--=-=-=- MELHORES CARDAPIOS -=-=-=--=-=-=--=-=-=-\n")
 
 # for pop in populacao:
 #    cont += 1
